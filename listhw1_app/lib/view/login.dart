9ededdee7f3b2be9876e33773ba9c5fe31d45979
sreeganeshhs <sreehariganeshs@gmail.com>
2020-05-12 13:59:00 +0530

    images

--- listhw1_app/lib/view/login.dart
+++ listhw1_app/lib/view/login.dart
@@ -1,13 +1,13 @@
 import 'homepage.dart';
 import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
-  var data =[{"name":"TONY STARK","age":"50","id":"01"},
-    {"name":"STEVE ROGERS","age":"104","id":"02"},
-    {"name":"THOR ODINSON","age":"2000","id":"03"},
-    {"name":"NATASHA ROMANOFF","age":"36","id":"04"},
-    {"name":"BRUCE BANNER","age":"47","id":"05"},
-    {"name":"T'CHALA","age":"37","id":"06"},
-    {"name":"CLINT BARTON","age":"40","id":"07"}];
+  var data =[{"image":"ts1.png","name":"TONY STARK","age":"50","id":"01"},
+    {"image":"Chris2.png","name":"STEVE ROGERS","age":"104","id":"02"},
+    {"image":"thor.jpg","name":"THOR ODINSON","age":"2000","id":"03"},
+    {"image":"natasha romanoff.jpg","name":"NATASHA ROMANOFF","age":"36","id":"04"},
+    {"image":"banner.jpg","name":"BRUCE BANNER","age":"47","id":"05"},
+    {"image":"t'chala.jpg","name":"T'CHALA","age":"37","id":"06"},
+    {"image":"Hawkeye.jpg","name":"CLINT BARTON","age":"40","id":"07"}];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,7 +30,14 @@ class Login extends StatelessWidget {
                     Card(
                       elevation: 10.0,
                       child: ListTile(
-                      leading: Icon(Icons.people,size: 40.0,),
+                      leading:
+                      ClipOval(
+                        child: Image(
+                          height: 50.0,
+                          width: 50.0,
+                          image: AssetImage(data[index]["image"]),
+                        ),
+                      ),
                       trailing: int.parse(data[index]["age"].toString()) > 50 ? FlatButton(
                         onPressed:(){
                         print("Clicked");
